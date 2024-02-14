7d9f0036e4bb9077bbb9adfa181af89987455836
Sergius92739 <sergem279@gmail.com>
2024-02-14 16:35:35 +0300

    api added && product.entity changed

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,6 +1,5 @@
 import { CategoryEntity } from "src/categories/entities/category.entity";
 import { OrdersProductsEntity } from "src/orders/entities/orders-products.entity";
-// import { OrdersProductsEntity } from "src/orders/entities/orders-products.entity";
 import { ReviewEntity } from "src/reviews/entities/review.entity";
 import { UserEntity } from "src/users/entities/user.entity";
 import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
